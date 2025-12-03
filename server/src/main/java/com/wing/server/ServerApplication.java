45c50b6922e927b0e6c06d437091b2408a9fe890
Wing_WR <wingwr998@gmail.com>
2025-12-03 14:33:50 +0800

    feat: 完成后端项目构建

---
+++ server/src/main/java/com/wing/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.wing.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
