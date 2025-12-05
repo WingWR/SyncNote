1be1d6bced8f8bd0e8ba35040a4029513881658f
Wing_WR <wingwr998@gmail.com>
2025-12-05 16:11:03 +0800

    feat: 构建侧边栏基本组件结构

---
+++ web_client/src/components/CoreSlideBar.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex h-screen bg-black overflow-hidden">
+    <NaviRail />
+  </div>
+</template>
+
+<script setup lang="ts">
+import NaviRail from './SystemSlideBar/NaviRail.vue';
+
+</script>
---
+++ web_client/src/views/Homepage.vue
@@ -0,0 +1,8 @@
+<template>
+    <CoreSlideBar />
+</template>
+
+<script setup lang="ts">
+import CoreSlideBar from '../components/CoreSlideBar.vue';
+
+</script>
