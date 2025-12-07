ce55fcafc71b71c1bafe7ad6471779480a0a8881
Wing_WR <wingwr998@gmail.com>
2025-12-07 22:26:44 +0800

    feat: 添加用户操作的Mapper层

---
+++ server/syncnote-user/src/main/java/com/syncnote/user/mapper/UserMapper.java
@@ -0,0 +1,9 @@
+package com.syncnote.user.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.syncnote.user.model.User;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper extends BaseMapper<User> {
+}
