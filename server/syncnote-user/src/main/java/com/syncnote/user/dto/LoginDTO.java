9c3d28e57cb7559a8ac51e64fe71e759f7253421
Wing_WR <wingwr998@gmail.com>
2025-12-07 22:21:20 +0800

    feat: 添加用户操纵的DTO

---
+++ server/syncnote-user/src/main/java/com/syncnote/user/dto/LoginDTO.java
@@ -0,0 +1,16 @@
+package com.syncnote.user.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class LoginDTO {
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String password;
+}
---
+++ server/syncnote-user/src/main/java/com/syncnote/user/dto/UpdateUserDTO.java
@@ -0,0 +1,9 @@
+package com.syncnote.user.dto;
+
+import lombok.Data;
+
+@Data
+public class UpdateUserDTO {
+    private String username;
+    private String avatar;
+}
