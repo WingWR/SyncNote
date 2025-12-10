ed1fb824f03c76151b00ad0b1e7e157db3684b9a
Wing_WR <wingwr998@gmail.com>
2025-12-10 17:17:33 +0800

    feat: 添加全局Controller异常处理

---
+++ server/syncnote-util/src/main/java/com/syncnote/util/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.syncnote.util;
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ApiResponse<?> handleRuntimeException(RuntimeException e) {
+        return ApiResponse.fail(400, e.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ApiResponse<?> handleException(Exception e){
+        return ApiResponse.fail(500, "系统内部异常");
+    }
+}
