f6105dea0cb514ffd5515e6d6d7e24d6a7ed7d45
qiankun25 <18864960592@163.com>
2025-12-18 07:30:11 +0800

    fix(util): 修复 Redis 连接失败错误，填充密码连接

--- server/syncnote-util/src/main/java/com/syncnote/util/RedisConfig.java
+++ server/syncnote-util/src/main/java/com/syncnote/util/RedisConfig.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
@@ -18,28 +20,33 @@ public class RedisConfig {
     @Value("${spring.data.redis.port}")
     private int redisPort;
 
+    @Value("${spring.data.redis.password}") // 1. 注入配置文件中的密码
+    private String redisPassword;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        // 使用配置属性创建并返回连接工厂
-        return new LettuceConnectionFactory(redisHost, redisPort);
+        // 2. 创建单机配置对象
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(redisHost);
+        config.setPort(redisPort);
+        config.setPassword(RedisPassword.of(redisPassword)); // 3. 关键：设置密码
+
+        // 4. 使用配置创建工厂
+        return new LettuceConnectionFactory(config);
     }
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
-
-        // 1. 设置连接工厂 (必须)
         template.setConnectionFactory(connectionFactory);
 
-        // 2. 配置 Key 的序列化器：使用 StringRedisSerializer，确保 Redis 中的 Key 是可读的字符串
         StringRedisSerializer stringSerializer = new StringRedisSerializer();
         template.setKeySerializer(stringSerializer);
-        template.setHashKeySerializer(stringSerializer); // Hash 结构的 Key 也使用 String
+        template.setHashKeySerializer(stringSerializer);
 
-        // 3. 配置 Value 的序列化器：使用 GenericJackson2JsonRedisSerializer，将 Java 对象转换为 JSON 字符串存储
         GenericJackson2JsonRedisSerializer jsonSerializer = new GenericJackson2JsonRedisSerializer();
         template.setValueSerializer(jsonSerializer);
-        template.setHashValueSerializer(jsonSerializer); // Hash 结构的 Value 也使用 JSON
+        template.setHashValueSerializer(jsonSerializer);
 
         template.afterPropertiesSet();
         return template;
