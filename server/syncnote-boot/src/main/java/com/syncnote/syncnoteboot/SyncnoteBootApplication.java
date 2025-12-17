b7967f608002b0ccd5ac04ded45538145a56fac3
Wing_WR <wingwr998@gmail.com>
2025-12-17 16:17:29 +0800

    feat: 启动类添加mapper扫描

--- server/syncnote-boot/src/main/java/com/syncnote/syncnoteboot/SyncnoteBootApplication.java
+++ server/syncnote-boot/src/main/java/com/syncnote/syncnoteboot/SyncnoteBootApplication.java
@@ -1,8 +1,10 @@
 package com.syncnote.syncnoteboot;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.syncnote")
 @SpringBootApplication(scanBasePackages = "com.syncnote")
 public class SyncnoteBootApplication {
     public static void main(String[] args) {
