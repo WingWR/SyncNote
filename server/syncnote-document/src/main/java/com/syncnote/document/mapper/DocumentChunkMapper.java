10f6e8262b7f23d01c8a5573a4b8749e8da7017a
Wing_WR <wingwr998@gmail.com>
2025-12-21 03:24:34 +0800

    refactor: 修改Chunk的MyBatis-PlusMapper层

--- server/syncnote-document/src/main/java/com/syncnote/document/mapper/DocumentChunkMapper.java
+++ server/syncnote-document/src/main/java/com/syncnote/document/mapper/DocumentChunkMapper.java
@@ -1,9 +1,10 @@
 package com.syncnote.document.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.syncnote.document.model.DocumentChunk;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
-public interface DocumentChunkMapper extends BaseMapper<Mapper> {
+public interface DocumentChunkMapper extends BaseMapper<DocumentChunk> {
 
 }
